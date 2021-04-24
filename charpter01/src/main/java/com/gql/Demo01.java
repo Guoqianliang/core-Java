e52a3f2353f6ef34ade4a97166f0d8a055359a18
gql <guoqianliang@foxmail.com>
2021-04-24 20:59:51 +0800

    Initial commit

---
+++ charpter01/src/main/java/com/gql/Demo01.java
@@ -0,0 +1,13 @@
+package com.gql;
+
+/**
+ * @Description:
+ * @author Guoqianliang
+ * @date 20:58 - 2021/4/24
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        args = new String[]{"好想吃田西瓜~"};
+        System.out.println(args[0]);
+    }
+}
